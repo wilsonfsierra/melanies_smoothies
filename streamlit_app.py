df8b6d4ff6205266ee48161620d3b93634849ec2
Sierra Wilson <sierra.funte@cellularsales.com>
2025-12-10 15:32:02 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ st.write('The name on your Smoothie will be:', name_on_order)
 
 cnx = st.connection('snowflake')
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('FRUIT_ID'))
 pd_df = my_dataframe.to_pandas()
 # st.dataframe(pd_df)
 # st.stop()
