311a29d3ff1de349c4dbab36bc3fea33cf3d97cc
Sierra Wilson <sierra.funte@cellularsales.com>
2025-12-10 14:15:26 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,8 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie")
 st.write("The name on your Smoothie will be:", name_on_order)
 
-session = Session.builder.configs(st.secrets["snowflake"]).create()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
 
