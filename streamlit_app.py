a7a504dbde295a883828fac240ff254455752674
Sierra Wilson <sierra.funte@cellularsales.com>
2025-12-10 15:32:25 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ if ingredients_list:
     for fruit in ingredients_list:
       ingredients_string += fruit + ' '
 
-      search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit, 'SEARCH_ON'].iloc[0]
+      search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit, 'FRUIT_ID'].iloc[0]
       st.write('The search value for ', fruit,' is ', search_on, '.')
 
       smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
